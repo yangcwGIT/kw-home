358fd5f5186bf110114cec2bd321a91bf83b5d63
Hancoson <hancoson@163.com>
2015-07-10 17:43:50 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,189 @@
+/**
+ * Created by Guoxing.han on 2015/7/7 0007.
+ */
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //clean: {
+        //    all: ['dist/*']
+        //},
+
+
+        jshint    : {
+            //files: ['src/pages/<%= grunt.config.get("page") %>/*.js'],
+            files: {
+                options: {
+                    force  : true,
+                    globals: {
+                        jQuery : true,
+                        console: true,
+                        module : true
+                    }
+                },
+                src    : ['scripts/index.js']
+            }
+
+        },
+        copy      : {
+
+            files: {
+                src   : [
+                    'bower_components/**/**/jquery.min.js',
+                    'bower_components/jquery/dist/jquery.min.map',
+                    'bower_components/**/**/js/swiper.jquery.min.js',
+                    'bower_components/**/**/js/maps/swiper.jquery.min.js.map',
+                    'bower_components/**/**/css/swiper.min.css',
+                    'favicon.ico'
+                ],
+                dest  : 'dist/',
+                filter: 'isFile'
+            }
+
+        },
+        uglify    : {
+            common: {
+
+                options: {
+                    banner: '/* \n *@name: <%= pkg.name %>\n *@author: <%= pkg.author %>\n *@date: <%= grunt.template.today("dd-mm-yyyy") %>\n*/\n'
+                },
+                files  : [{
+                    expand: true,
+                    src   : ['scripts/*.js'],
+                    dest  : 'dist',
+                    ext   : '-<%= pkg.timestamp %>.js'
+                }]
+            }
+        },
+        cssmin    : {
+
+            common: {
+                options: {
+                    banner: '/* \n *@name: <%= pkg.name %>\n *@author: <%= pkg.author %>\n *@date: <%= grunt.template.today("dd-mm-yyyy") %>\n*/\n'
+                },
+                files  : [{
+                    expand: true,
+                    src   : ['styles/*.css'],
+                    dest  : 'dist',
+                    ext   : '-<%= pkg.timestamp %>.css'
+                }]
+            }
+        },
+        imagemin  : {
+            common: {
+                files: [{
+                    expand: true,
+                    src   : ['images/*.{png,jpg,gif,jpeg}', 'images/**/*.{png,jpg,gif,jpeg}'],
+                    dest  : 'dist'
+                }]
+            }
+
+        },
+        dom_munger: {
+            index  : {
+                options: {
+                    remove : ['script[data-remove!="false"]', 'link[data-remove!="false"]'],
+                    append : [
+                        {
+                            selector: 'body',
+                            html    : '<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>' +
+                            '<script type="text/javascript" src="scripts/index-<%= pkg.timestamp %>.js"></script>' +
+                            '<script type="text/javascript" src="scripts/jquery-fadethis-<%= pkg.timestamp %>.js"></script>'
+
+                        }
+                    ],
+                    prepend: [
+                        {
+                            selector: 'head',
+                            html    : '<link rel="stylesheet" href="styles/reset-<%= pkg.timestamp %>.css" type="text/css"/>' + '<link rel="stylesheet" href="styles/index-<%= pkg.timestamp %>.css" type="text/css"/>'
+                        }
+                    ]
+                },
+                src    : 'index.html',
+                dest   : 'dist/index.html'
+            },
+            about  : {
+                options: {
+                    remove : ['script[data-remove!="false"]', 'link[data-remove!="false"]'],
+                    append : [
+                        {
+                            selector: 'body',
+                            html    : '<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>' +
+                            '<script type="text/javascript" src="bower_components/swiper/dist/js/swiper.jquery.min.js"></script>' +
+                            '<script type="text/javascript" src="scripts/index-<%= pkg.timestamp %>.js"></script>'
+                        }
+                    ],
+                    prepend: [
+                        {
+                            selector: 'head',
+                            html    : '<link rel="stylesheet" href="styles/reset-<%= pkg.timestamp %>.css" type="text/css"/>' +'<link rel="stylesheet" href="bower_components/swiper/dist/css/swiper.min.css" type="text/css"/>' + '<link rel="stylesheet" href="styles/index-<%= pkg.timestamp %>.css" type="text/css"/>'
+                        }
+                    ]
+                },
+                src    : 'about.html',
+                dest   : 'dist/about.html'
+            },
+            contact: {
+                options: {
+                    remove : ['script[data-remove!="false"]', 'link[data-remove!="false"]'],
+                    append : [
+                        {
+                            selector: 'body',
+                            html    : '<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>' +
+                            '<script type="text/javascript" src="scripts/superslide-<%= pkg.timestamp %>.js"></script>'+
+                            '<script type="text/javascript" src="scripts/index-<%= pkg.timestamp %>.js"></script>'
+
+                        }
+                    ],
+                    prepend: [
+                        {
+                            selector: 'head',
+                            html    : '<link rel="stylesheet" href="styles/reset-<%= pkg.timestamp %>.css" type="text/css"/>' + '<link rel="stylesheet" href="styles/index-<%= pkg.timestamp %>.css" type="text/css"/>'
+                        }
+                    ]
+                },
+                src    : 'contact.html',
+                dest   : 'dist/contact.html'
+            },
+
+        },
+
+        htmlmin: {
+            options: {
+                removeComments    : true,
+                collapseWhitespace: true
+            },
+            html   : {
+                files: [{
+                    expand: true,
+                    cwd   : 'dist',
+                    src   : ['*.html'],
+                    dest  : 'dist'
+                }]
+            }
+        },
+
+    });
+
+    /**
+     * 载入使用到的通过NPM安装的模块
+     */
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    /**
+     * 注册基本任务
+     */
+
+    grunt.registerTask('build', [
+        'jshint:files',
+        'copy:files',
+        'uglify:common',
+        'imagemin:common',
+        'cssmin:common',
+        'dom_munger:index',
+        'dom_munger:about',
+        'dom_munger:contact',
+        'htmlmin'
+    ]);
+    grunt.registerTask('dist', ['build']);
+}
---
+++ scripts/index.js
@@ -0,0 +1,176 @@
+/**
+ * Created by Guoxing.han on 2015/7/7 0007.
+ */
+jQuery(function () {
+    var indexHome = {
+        init: function () {
+            this.eventHandler();
+            if ($("#aboutPage").length > 0) {
+                this.aboutPage();
+            }
+            if ($("#contactPage").length > 0) {
+                this.contactPage();
+            }
+        }
+    };
+
+    jQuery.extend(indexHome, {
+        eventHandler: function () {
+            var winHeight = $(document).scrollTop();
+            $(window).scroll(function () {
+                var scrollY = $(document).scrollTop();// 获取垂直滚动的距离，即滚动了多少
+                if (scrollY > 250) { //如果滚动距离大于550px则隐藏，否则删除隐藏类
+                    $('#J_header').addClass('hiddened');
+                }
+                else {
+                    $('#J_header').removeClass('hiddened');
+                }
+                if (scrollY > winHeight) { //如果没滚动到顶部，删除显示类，否则添加显示类
+                    $('#J_header').removeClass('showed');
+                }
+                else {
+                    $('#J_header').addClass('showed');
+                }
+                winHeight=scrollY;
+            });
+
+            $(window).scroll(function () {
+                if (jQuery(window).scrollTop() >= 100) {
+                    jQuery('#updown').fadeIn(300);
+                } else {
+                    jQuery('#updown').fadeOut(300);
+                }
+            });
+            jQuery('#updown .up').click(function () {
+                jQuery('html,body').animate({scrollTop: '0px'}, 800);
+            });
+            $("#J_brain-left").animate({"right": "185px", "opacity": 1}, 600);
+            $("#J_brain-right").animate({"right": "47px", "opacity": 1}, 600);
+            setTimeout(function () {
+                $("#J_elec-box,#J_sq-right,#J_d-left,#J_elec-icon").animate({"opacity": 1}, 700);
+                $("#J_sq-right,#J_elec-icon").addClass("animation");
+                $(".j_line-bar").animate({"opacity": 0.07});
+                $(".j_line-corner").animate({"opacity": 1});
+            }, 800);
+            $("#J_weixin-btn").hover(function () {
+                $("#J_weixin-coad").fadeIn(200);
+            }, function () {
+                $("#J_weixin-coad").fadeOut(200);
+            });
+            $(".j_downApp-btn").click(function () {
+                $("html,body").stop(true);
+                $("html,body").animate({scrollTop: $("#J_downApp-box").offset().top}, 1000);
+            });
+            if($("#indexPage").length>0){
+                $(window).fadeThis({
+                    speed: 1000,
+                    distance: 400,
+                    reverse: false
+                });
+            }
+        },
+        aboutPage   : function () {
+            $(".j_close-btn").on("click", function () {
+                $("#J_alert_win").fadeOut(300);
+            });
+            $("#J_open-play").on("click", function () {
+                $("#J_alert_win").fadeIn(300);
+            });
+            var swiper = new Swiper('.swiper-container', {
+                autoplay                    : 3000,
+                autoplayDisableOnInteraction: false,
+                loop                        : true,
+                effect                      : 'fade',
+                //onTransitionStart: function(index,slide){
+                //    console.log(index,slide)
+                //}
+                onTransitionStart: function(swiper){
+                    //console.log(swiper.activeIndex);
+                    if(swiper.activeIndex<=5||swiper.activeIndex==11){
+                        $("#J_swiper-box>.left-box").addClass("active");
+                        $("#J_swiper-box>.right-box").removeClass("active");
+                    }
+                    else{
+                        $("#J_swiper-box>.right-box").addClass("active");
+                        $("#J_swiper-box>.left-box").removeClass("active");
+                    }
+                    if(swiper.activeIndex==11){
+                        $("#J_swiper-box i").animate({"opacity":0},300);
+                        $("#J_swiper-box .icson1").animate({"opacity":1},300);
+                    }
+                    else{
+                        $("#J_swiper-box i").animate({"opacity":0},300);
+                        $("#J_swiper-box .icson"+swiper.activeIndex).animate({"opacity":1},300);
+                    }
+                }
+
+            });
+            $('#J_swiper-box').on("mouseenter","a",function(){
+                var _val = $(this).attr("data-val");
+                swiper.slideTo(_val, 300, false);
+                $("#J_swiper-box i").animate({"opacity":0},300);
+                $("#J_swiper-box .icson"+_val).animate({"opacity":1},300);
+                if(_val<=5||_val==11){
+                    $("#J_swiper-box>.left-box").addClass("active");
+                    $("#J_swiper-box>.right-box").removeClass("active");
+                }
+                else{
+                    $("#J_swiper-box>.right-box").addClass("active");
+                    $("#J_swiper-box>.left-box").removeClass("active");
+                }
+            });
+
+        },
+        contactPage : function () {
+            var _height = $("#J_contact-photo>a").width() * 393 / 495, _allW = document.body.clientWidth;
+            $("#J_contact-photo>a").height(_height);
+            if (_allW >= 1500) {
+                $("#J_contact-photo>a").hover(function () {
+                    $("em.opacity", this).animate({"top": "100%"}, 500);
+                    $("em.bottom", this).animate({"top": "86%"}, 500);
+                    $("p", this).animate({"top": "86%"}, 500);
+                    $("p>span.s", this).fadeOut(600);
+                }, function () {
+                    $("em.opacity", this).animate({"top": 0}, 500);
+                    $("em.bottom", this).animate({"top": "100%"}, 500);
+                    $("p", this).animate({"top": "40%"}, 500);
+                    $("p>span.s", this).fadeIn(1);
+                });
+            }
+            else {
+                $("#J_contact-photo>a").hover(function () {
+                    $("em.opacity", this).animate({"top": "100%"}, 500);
+                    $("em.bottom", this).animate({"top": "83%"}, 500);
+                    $("p", this).animate({"top": "83%"}, 500);
+                    //setTimeout(function(){
+                    $("p>span.s", this).fadeOut(600);
+                    //},500)
+                }, function () {
+                    $("em.opacity", this).animate({"top": 0}, 500);
+                    $("em.bottom", this).animate({"top": "100%"}, 500);
+                    $("p", this).animate({"top": "40%"}, 500);
+                    $("p>span.s", this).fadeIn(1);
+                });
+            }
+
+            $(".slider .bd li").first().before($(".slider .bd li").last());
+            $(".slider").hover(function () {
+                $(this).find(".arrow").stop(true, true).fadeIn(300);
+            }, function () {
+                $(this).find(".arrow").fadeOut(300);
+            });
+            $(".slider").slide({
+                titCell : ".hd ul",
+                mainCell: ".bd ul",
+                effect  : "leftLoop",
+                autoPlay: true,
+                vis     : 3,
+                autoPage: true,
+                trigger : "click"
+            });
+
+        }
+    });
+    indexHome.init();
+
+});
